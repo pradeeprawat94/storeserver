a5c0930f144e01ed8c746286fa012170cc124b28
pradeeprawat94 <bablu.rawat94@gmail.com>
2023-01-31 08:51:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import  Express  from "express";
+import cors from 'cors'
+import bodyParser from "body-parser";
+import connectToDatabase from './database/database.js'
+import productRoute from './routes/productRoutes.js'
+import dotenv from 'dotenv'
+
+
+const app = Express()
+
+dotenv.config();
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use('/',productRoute)
+
+connectToDatabase(process.env.PASSWORD,process.env.APP);
+app.listen(process.env.PORT,()=>{
+    console.log("Sever is running at " ,process.env.PORT)
+})
